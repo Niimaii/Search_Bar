5a95c479d62fd680b439f303b5d56d9f16063d29
Niimaii <nimaicgomes@gmail.com>
2023-05-25 20:07:02 -0300

    Search Bar logic

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,11 +1,69 @@
+import { useState } from 'react';
+import './SearchBar.css';
+import SearchIcon from '@mui/icons-material/Search';
+import CloseIcon from '@mui/icons-material/Close';
+
 function SearchBar({ placeholder, data }) {
+  const [filteredData, setFilteredData] = useState([]);
+  const [wordEntered, setWordEntered] = useState('');
+
+  const handleFilter = (e) => {
+    const searchWord = e.target.value;
+    setWordEntered(searchWord);
+    // Filter through each data set
+    const newFilter = data.filter((value) => {
+      // If the data set (title) includes the input word then return it
+      //   Set them to lowercase so capitalization doesn't affect searches
+      return value.title.toLowerCase().includes(searchWord.toLowerCase());
+    });
+
+    if (searchWord === '') {
+      setFilteredData([]);
+    } else {
+      setFilteredData(newFilter);
+    }
+  };
+
+  const clearInput = () => {
+    setFilteredData([]);
+    setWordEntered('');
+  };
   return (
     <div className='search'>
       <div className='searchInputs'>
-        <input type='text' placeholder={placeholder} />
-        <div className='searchIcon'></div>
+        <input
+          type='text'
+          placeholder={placeholder}
+          onChange={handleFilter}
+          value={wordEntered}
+        />
+        <div className='searchIcon'>
+          {filteredData.length === 0 ? (
+            <SearchIcon />
+          ) : (
+            <CloseIcon id='clearBtn' onClick={clearInput} />
+          )}
+        </div>
       </div>
-      <div className='dataResult'></div>
+
+      {filteredData.length != 0 && (
+        <div className='dataResult'>
+          {/* Slice the data to save on performance */}
+          {filteredData.slice(0, 15).map((value, key) => {
+            return (
+              <a
+                className='dataItem'
+                href={value.link}
+                rel='noreferrer'
+                target='_blank'
+                key={key}
+              >
+                <p>{value.title}</p>
+              </a>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
