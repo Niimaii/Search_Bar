4a54f13bd28ec5afd3446f5e27647dd07fa8427c
Niimaii <nimaicgomes@gmail.com>
2023-05-25 18:48:49 -0300

    Search Bar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import SearchBar from './Components/SearchBar';
+import Data from './Data.json';
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <SearchBar placeholder='Enter A Book Name...' data={Data} />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,5 +1,13 @@
-function SearchBar() {
-  return <div>SearchBar</div>;
+function SearchBar({ placeholder, data }) {
+  return (
+    <div className='search'>
+      <div className='searchInputs'>
+        <input type='text' placeholder={placeholder} />
+        <div className='searchIcon'></div>
+      </div>
+      <div className='dataResult'></div>
+    </div>
+  );
 }
 
 export default SearchBar;
